55acfc89ed8d9d80adc7c06137fa53e3c9c91906
calmantara <calmantarasp@gmail.com>
2024-03-24 15:16:20 +0700

    add env variables

--- app/main/configuration/config.py
+++ app/main/configuration/config.py
@@ -21,8 +21,18 @@ class Config:
 
 
 class Development(Config):
+    postgres_password = os.getenv("POSTGRES_PASSWORD")
+    postgres_username = os.getenv("POSTGRES_USERNAME")
+    postgres_host = os.getenv("POSTGRES_HOST")
+
     SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:mysecretpassword@127.0.0.1:15432/py_account"
+        "postgresql://"
+        + postgres_username
+        + ":"
+        + postgres_password
+        + "@"
+        + postgres_host
+        + "/py_account"
     )
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -33,7 +43,19 @@ class Development(Config):
 #     SQLALCHEMY_ECHO = False
 #     SQLALCHEMY_TRACK_MODIFICATIONS = False
 class Production(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:xlQVToCslyLpcogELwftgiFYdrprVnSe@monorail.proxy.rlwy.net:10545/railway"
+    postgres_password = os.getenv("POSTGRES_PASSWORD")
+    postgres_username = os.getenv("POSTGRES_USERNAME")
+    postgres_host = os.getenv("POSTGRES_HOST")
+
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://"
+        + postgres_username
+        + ":"
+        + postgres_password
+        + "@"
+        + postgres_host
+        + "/py_account"
+    )
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
